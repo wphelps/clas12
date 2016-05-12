639064026f333b15a2018b7bc3981ccd2f484eb4
Will Phelps <willphelps@jlab.org>
2016-05-12 18:08:35 -0400

    Update
    Quite a few updates from gavalian/master

---
+++ jroot/src/main/java/org/root/ui/ParameterPanel.java
@@ -0,0 +1,86 @@
+package org.root.ui;
+
+import java.awt.GridLayout;
+import java.util.ArrayList;
+
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+import org.root.basic.EmbeddedCanvas;
+import org.root.func.F1D;
+
+public class ParameterPanel extends JPanel{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3175511650738525339L;
+	int index;
+	EmbeddedCanvas canvas;
+	F1D fitFunction;
+	ArrayList<JPanel> parameterPanels = new ArrayList<JPanel>();
+	ArrayList<JCheckBox> parameterLimited = new ArrayList<JCheckBox>();
+	ArrayList<JCheckBox> parameterFixed = new ArrayList<JCheckBox>();
+	ArrayList<JTextField> parameterMin = new ArrayList<JTextField>();
+	ArrayList<JTextField> parameterMax = new ArrayList<JTextField>();
+	ArrayList<JTextField> parameterName = new ArrayList<JTextField>();
+	ArrayList<JTextField> parameterValue = new ArrayList<JTextField>();
+	ArrayList<JSlider> parameterValueSliders = new ArrayList<JSlider>();
+
+	public ParameterPanel(EmbeddedCanvas canvas, int index, F1D func){
+		this.index=index;
+		this.fitFunction = func;
+		this.canvas = canvas;
+		this.setLayout(new GridLayout(func.getNParams(),1));
+		initParameterPanels();
+	}
+	private void initParameterPanels(){
+		parameterPanels = new ArrayList<JPanel>();
+		ArrayList<JCheckBox> parameterLimitedCheckboxes = new ArrayList<JCheckBox>();
+		for(int i=0; i<fitFunction.getNParams(); i++){
+			parameterPanels.add(new JPanel());
+			parameterLimited.add(new JCheckBox("Lim."));
+			parameterLimited.get(i).setSelected(fitFunction.parameter(i).isLimited());
+			parameterFixed.add(new JCheckBox("Fix"));
+			parameterValue.add(new JTextField(String.format("%4.2f", fitFunction.parameter(i).value())));
+			if(parameterLimited.get(i).isSelected()){
+				parameterMin.add(new JTextField(String.format("%4.2f", fitFunction.parameter(i).min())));
+				parameterMax.add(new JTextField(String.format("%4.2f", fitFunction.parameter(i).max())));
+			}else{
+				parameterMin.add(new JTextField(String.format("%4.2f", fitFunction.parameter(i).value()/10.0)));
+				parameterMax.add(new JTextField(String.format("%4.2f", fitFunction.parameter(i).value()*10.0)));
+			}
+			parameterValueSliders.add(new JSlider());
+			parameterName.add(new JTextField(fitFunction.parameter(i).name()));
+			
+			parameterPanels.get(i).add(parameterLimited.get(i));
+			parameterPanels.get(i).add(parameterFixed.get(i));
+			parameterPanels.get(i).add(parameterMin.get(i));
+			parameterPanels.get(i).add(parameterValueSliders.get(i));
+			parameterPanels.get(i).add(parameterMax.get(i));
+			this.add(parameterPanels.get(i));
+		}		
+	}
+	public void updateNewFunction(F1D func){
+		this.fitFunction = func;
+		this.removeAll();
+		this.setLayout(new GridLayout(func.getNParams(),1));
+		parameterPanels.clear();
+		parameterLimited.clear();
+		parameterFixed.clear();
+		parameterMin.clear();
+		parameterMax.clear();
+		parameterName.clear();
+		parameterValue.clear();
+		parameterValueSliders.clear();
+		initParameterPanels();
+		//this.setPreferredSize(this.getPreferredSize());
+		this.revalidate();
+		this.repaint();
+		JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
+		topFrame.pack();
+	}
+}
